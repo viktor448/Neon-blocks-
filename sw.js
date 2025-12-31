a1500a969021f756befdbded7559cd4182ed2414
viktor448 <116098111+viktor448@users.noreply.github.com>
2025-12-31 13:45:20 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'neon-blocks-v3';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
